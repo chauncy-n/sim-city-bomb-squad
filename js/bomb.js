cb8fbe3a9eeaf35a55c8a4ff1fe8f53b69f7a5b9
chauncy-n <eldoon@comcast.net>
2018-09-14 12:42:12 -0700

    finished laying out UI

--- js/bomb.js
+++ js/bomb.js
@@ -1,5 +1,14 @@
 console.log("javascript running");
 
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
 });
